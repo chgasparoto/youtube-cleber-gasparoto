54c0c3a0d91935b4592ec3639d70707e0bb2162b
Cleber Gasparoto <chgasparoto@gmail.com>
2021-04-19 05:54:05 +0200

    convert output locals to object

--- terraform-time-and-date-functions/tf/main.tf
+++ terraform-time-and-date-functions/tf/main.tf
@@ -4,14 +4,10 @@ terraform {
 
 resource "random_pet" "bucket" {}
 
-output "pet" {
-  value = random_pet.bucket.id
-}
-
 locals {
-  now  = timestamp()
+  now = timestamp()
 
-  brasilia_tz = timeadd(local.now, "-3h")
+  brasilia_tz  = timeadd(local.now, "-3h")
   amsterdam_tz = timeadd(local.now, "2h")
 
   date_br = formatdate("DD/MM/YYYY", local.brasilia_tz)
@@ -20,19 +16,21 @@ locals {
   date_utc = formatdate("YYYY-MM-DD", local.now)
 }
 
-output "locals" {
-  value = [
-    local.now,
-
-    local.brasilia_tz,
-    local.amsterdam_tz,
-
-    local.date_br,
-    local.date_nl,
-    local.date_utc
-  ]
-}
-
 output "bucket_name" {
   value = "${random_pet.bucket.id}-${local.date_utc}"
+}
+
+output "locals" {
+  value = {
+    "now"          = local.now,
+    "brasilia_tz"  = local.brasilia_tz,
+    "amsterdam_tz" = local.amsterdam_tz,
+    "date_br"      = local.date_br,
+    "date_nl"      = local.date_nl,
+    "date_utc"     = local.date_utc
+  }
+}
+
+output "pet" {
+  value = random_pet.bucket.id
 }
