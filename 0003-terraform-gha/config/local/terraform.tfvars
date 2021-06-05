d7e08645aa193f5d54daa8b6c20f702b16ae36ac
Cleber Gasparoto <chgasparoto@gmail.com>
2021-06-05 08:22:50 +0200

    add backend/provider prop in terraform block; add local env

---
+++ 0003-terraform-gha/config/local/terraform.tfvars
@@ -0,0 +1,3 @@
+aws_profile   = "tf014"
+environment   = "local"
+instance_type = "t3.small"
--- 0003-terraform-gha/main.tf
+++ 0003-terraform-gha/main.tf
@@ -1,5 +1,14 @@
 terraform {
   required_version = "0.15.5"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "3.44.0"
+    }
+  }
+
+  backend "s3" {}
 }
 
 provider "aws" {
