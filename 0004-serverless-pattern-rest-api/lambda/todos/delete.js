e0d83b6e126d7e3fedbddcf446f0d62c85a3b578
Cleber Gasparoto <chgasparoto@gmail.com>
2021-08-31 17:21:41 +0200

    add lambdas code

---
+++ 0004-serverless-pattern-rest-api/lambda/todos/delete.js
@@ -0,0 +1,44 @@
+const AWS = require('aws-sdk');
+const dynamo = new AWS.DynamoDB.DocumentClient();
+
+const normalizeEvent = require('./normalizer');
+const response = require('./response');
+
+exports.handler = async event => {
+  if (process.env.DEBUG) {
+    console.log({
+      message: 'Received event',
+      data: JSON.stringify(event),
+    });
+  }
+
+  const table = event.table || process.env.TABLE;
+  if (!table) {
+    throw new Error('No table name defined.');
+  }
+
+  const {
+    data: { id },
+  } = normalizeEvent(event);
+
+  const params = {
+    TableName: table,
+    Key: {
+      id: parseInt(id, 10),
+    },
+  };
+
+  try {
+    await dynamo.delete(params).promise();
+
+    console.log({
+      message: 'Record has been deleted',
+      data: JSON.stringify(params),
+    });
+
+    return response(200, `Record ${id} has been deleted`);
+  } catch (err) {
+    console.error(err);
+    return response(500, 'Somenthing went wrong');
+  }
+};
