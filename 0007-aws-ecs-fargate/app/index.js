556a1ac889b6b32283ee302e9193e521f328cb8c
Cleber Gasparoto <chgasparoto@gmail.com>
2023-01-16 23:26:08 +0100

    update code to interact with rds

--- 0007-aws-ecs-fargate/app/index.js
+++ 0007-aws-ecs-fargate/app/index.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const axios = require('axios');
 
+const db = require('./db');
+
 const app = express();
 const port = 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 const indexPage = `
     <h3>Hello from a Node.js Application running on AWS ECS Fargate</h3>
@@ -10,6 +15,8 @@ const indexPage = `
     <ul>
         <li>Random dogs? <a href="/dogs">Click here</a></li>
         <li>Random cats? <a href="/cats">Click here</a></li>
+        <li>Create tables on AWS RDS <a href="/create-tables">Click here</a></li>
+        <li>Check my todo list <a href="/todos">Click here</a></li>
     </ul>
 `;
 
@@ -49,6 +56,56 @@ app.get('/cats', async (req, res) => {
     res.send(
       `<img src="${catImage}" alt="random cat" style="max-width: 500px" />`
     );
+  } catch (error) {
+    console.error(JSON.stringify(error));
+    res.status(500);
+    res.send(error.message);
+  }
+});
+
+app.get('/create-tables', async (req, res) => {
+  const createTableSql = `
+  CREATE EXTENSION IF NOT EXISTS "pgcrypto";
+  
+  CREATE TABLE IF NOT EXISTS todos (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    todo VARCHAR (255) NOT NULL,
+    done BOOLEAN DEFAULT false,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+  );`
+
+  try {
+    await db.query(createTableSql);
+    res.send('Todo table has been created');
+  } catch (error) {
+    console.error(JSON.stringify(error));
+    res.status(500);
+    res.send(error.message);
+  }
+});
+
+app.get('/todos', async(req, res) => {
+  try {
+    const result = await db.query('SELECT * FROM todos');
+
+    console.log({ result });
+
+    res.send(result.rows);
+  } catch (error) {
+    console.error(JSON.stringify(error));
+    res.status(500);
+    res.send(error.message);
+  }
+});
+
+app.post('/todos', async(req, res) => {
+  try {
+    const params = req.body;
+
+    console.log({ params });
+
+    const result = await db.query('INSERT INTO todos(todo) VALUES($1) RETURNING *', [params.todo]);
+    res.send(result.rows[0]);
   } catch (error) {
     console.error(JSON.stringify(error));
     res.status(500);
